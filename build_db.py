6ca8e5e5786c0b6f1a4908e66b6e14941c3d45e9
Juan Carlos Zenteno <juancarlos_zp@ciencias.unam.mx>
2025-08-17 22:36:20 -0600

    Adding files to build the db in posit cloud

---
+++ build_db.py
@@ -0,0 +1,45 @@
+import duckdb
+
+connection = duckdb.connect("Registro_Global_Genes_y_Diagnosticos.duckdb")
+
+connection.execute(
+    "CREATE TABLE diseases (disease_id INTEGER PRIMARY KEY, name VARCHAR(200))"
+)
+connection.execute("""
+CREATE TYPE disease_category AS ENUM (
+    'cancer',
+    'cardiovascular',
+    'cerebrovascular',
+    'connective tissue',
+    'dermatological',
+    'endocrine',
+    'hematological, immunological and lymphatic',
+    'metabolic',
+    'muscular',
+    'neurodevelopmental',
+    'neurological',
+    'ocular',
+    'renal and genitourinary',
+    'skeletal'
+)""")
+connection.execute("""
+    CREATE TYPE inheritance_types AS ENUM (
+        'AD',
+        'AR',
+        'XL',
+        'MT'
+    )
+""")
+connection.execute("""
+    CREATE TABLE genes (
+        entry_id INTEGER PRIMARY KEY,
+        gene VARCHAR(60),
+        disease_id INTEGER,
+        omim INTEGER,
+        category disease_category,
+        inheritance VARCHAR(2),
+        somatism BOOLEAN,
+        observations VARCHAR(100),
+        FOREIGN KEY (disease_id) REFERENCES diseases(disease_id)
+    )
+""")
---
+++ sheet_processing.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import duckdb
+import math
+
+data = pd.read_csv("data.csv")
+db = duckdb.connect("Registro_Global_Genes_y_Diagnosticos.duckdb")
+
+diseases = sorted(data["DISEASE"].map(lambda x: x.strip()).unique())
+for idx, disease in enumerate(diseases):
+    print(disease)
+    db.execute("INSERT INTO diseases VALUES (?, ?)", [idx, disease])
+
+# gene_data = data[~data["GENES"].str.contains("p|q")]
+for idx, row in data.iterrows():
+    print(row)
+    gene = row["GENES"]
+
+    disease = row["DISEASE"]
+    disease = db.execute("SELECT * FROM diseases WHERE name LIKE ?", [disease.strip()]).df()["disease_id"].item()
+    
+    omim = row[" OMIM #"]
+    if math.isnan(omim):
+        omim = -1
+    
+    category = row["DISEASE CATEGORY"].strip().lower()
+    
+    inheritance = row["INHERITANCE"]
+    somatism = inheritance.endswith("(somatic)")
+    inheritance = inheritance.removesuffix("(somatic)").strip()
+    
+    observations = row["References for disease without OMIM code"]
+
+    db.execute(
+        "INSERT INTO genes VALUES (?,?,?,?,?,?,?,?)",
+        [   
+            idx,
+            gene,
+            disease,
+            omim,
+            category,
+            inheritance,
+            somatism,
+            observations
+        ]
+    )
